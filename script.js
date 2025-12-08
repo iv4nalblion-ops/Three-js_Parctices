d849e13971f94a2470a2816e4a9d03c274f48784
iv4nalblion-ops <iv4n.alblion@gmail.com>
2025-12-08 17:16:31 -0500

    changed rotation of the 3d objects

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 import * as THREE from "three";
 
 const canvas = document.querySelector("canvas.webgl");
@@ -6,15 +7,49 @@ console.log(canvas);
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color: 0xff0000});
-const mesh = new THREE.Mesh(geometry,material);
-scene.add(mesh);
+const group = new THREE.Group();
+scene.add(group);
+
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+)
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff})
+)
+group.add(cube3);
+
+cube1.position.x = -1.5;
+cube3.position.x = 1.5;
+
+group.position.y = 0;
+group.scale.y = 1;
+group.rotation.y = Math.PI * 0.25;
+
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+
+//mesh.position.set(0.7,-0.6,.5);
+//mesh.rotation.reorder("YXZ");
+//mesh.rotation.x = Math.PI * 0.25;
+
+
 
 const Sizes = {
 
     width: 800,
-    height: 600
+    height: 600 
 
 
 
@@ -28,13 +63,40 @@ const camera = new THREE.PerspectiveCamera(75,Sizes.width / Sizes.height);
 camera.position.z = 3;
 scene.add(camera);
 
+//renderer
+
 const renderer = new THREE.WebGLRenderer({
 
     canvas : canvas
+
+
+    
 
 
 
 });
 
 renderer.setSize(Sizes.width, Sizes.height);
-renderer.render(scene,camera);
+let time = Date.now();
+
+
+//animations
+const tick = () => {
+
+    //time
+    const currentTime = Date.now();
+    const DeltaTime = currentTime - time;
+    time = currentTime;
+    
+
+
+    cube1.rotation.x += 0.01 *DeltaTime;
+    cube2.rotation.y += 0.01;
+    cube3.rotation.z += 0.01;
+    renderer.render(scene,camera);
+    window.requestAnimationFrame(tick);
+
+
+}
+
+tick();
